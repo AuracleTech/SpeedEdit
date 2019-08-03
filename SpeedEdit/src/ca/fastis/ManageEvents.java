8e923b95bf894058321cc22aaa0f5a1316430b17
BuildTools <unconfigured@null.spigotmc.org>
2019-08-02 20:02:53 -0400

    Bug fix

--- SpeedEdit/src/ca/fastis/ManageEvents.java
+++ SpeedEdit/src/ca/fastis/ManageEvents.java
@@ -37,9 +37,10 @@ public class ManageEvents implements Listener{
 		Player player = event.getPlayer();
 		Block block = event.getClickedBlock();
 		if(player.hasPermission("speededit.use")) {
-			if (player.getInventory().getItemInMainHand().getType() == SpeedEdit.Tool && player.getInventory().getItemInMainHand().getItemMeta().hasCustomModelData() && player.getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == SpeedEdit.ToolID  && event.getHand().equals(EquipmentSlot.HAND) && (event.getAction().equals(Action.LEFT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK))) {
+			if ((event.getAction().equals(Action.LEFT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) && player.getInventory().getItemInMainHand().getType() == SpeedEdit.Tool && player.getInventory().getItemInMainHand().getItemMeta().hasCustomModelData() && player.getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == SpeedEdit.ToolID && event.getHand().equals(EquipmentSlot.HAND)) {
+				player.sendMessage(player.getInventory().getItemInMainHand().getType().toString());
 				int PosToSet = event.getAction().equals(Action.LEFT_CLICK_BLOCK) ? 1 : event.getAction().equals(Action.RIGHT_CLICK_BLOCK) ? 2 : 1;
-				SpeedEdit.getUser(player).setPosition(PosToSet, block.getLocation()); // refreshSelectionZone(player, 1);
+				SpeedEdit.getUser(player).setPosition(PosToSet, block.getLocation());
 				event.setCancelled(true);
 			}
 		}
