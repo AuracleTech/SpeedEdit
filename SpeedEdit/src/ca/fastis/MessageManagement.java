dfe6c025f8ea168370fafbd8e63508e34ffe05d6
BuildTools <unconfigured@null.spigotmc.org>
2019-08-09 06:55:16 -0400

    Removed extra space

--- SpeedEdit/src/ca/fastis/MessageManagement.java
+++ SpeedEdit/src/ca/fastis/MessageManagement.java
@@ -7,7 +7,7 @@ import net.md_5.bungee.api.ChatColor;
 public class MessageManagement {
 
 	public static void command(Player player, String string, String SpyMessage) {
-		player.sendMessage(ChatColor.GREEN + "SE " + ChatColor.DARK_GRAY + " " + string);
+		player.sendMessage(ChatColor.GREEN + "SE " + ChatColor.DARK_GRAY + string);
 		if(SpyMessage != null) for (Player online : SpeedEdit.server.getOnlinePlayers()) if (online.getUniqueId() != player.getUniqueId() && (online.isOp() || new ErrorManagement(online).hasPermission(online, "speededit.spy", false))) online.sendMessage(ChatColor.GREEN + "SE Spy " + ChatColor.DARK_GRAY + SpyMessage);
 	}
 
