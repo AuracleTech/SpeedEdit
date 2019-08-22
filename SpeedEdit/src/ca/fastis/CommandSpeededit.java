5c9833ced2f33e2272fd4d9a311885cbaefe4fde
BuildTools <unconfigured@null.spigotmc.org>
2019-08-21 21:53:23 -0400

    Final changes for SpeedEdit v2

--- SpeedEdit/src/ca/fastis/CommandSpeededit.java
+++ SpeedEdit/src/ca/fastis/CommandSpeededit.java
@@ -12,9 +12,9 @@ public class CommandSpeededit implements CommandExecutor {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			MessageManagement.command(player, "Speed edit version " + SpeedEdit.plugin.getDescription().getVersion(), null);
-			MessageManagement.command(player, "Made by Auracle#5220 (Discord)", null);
-			MessageManagement.command(player, "Report issues or request to https://github.com/VirgilFabre/SpeedEdit", null);
-			MessageManagement.command(player, "Donate to help me continue https://bit.ly/2zd4Lyj", null);
+			MessageManagement.command(player, "Made by §aAuracle§2#5220§7 (Discord)", null);
+			MessageManagement.command(player, "Report issues or request to §ehttps://bit.ly/2HjAW3x", null);
+			MessageManagement.command(player, "Donate to help me continue §ehttps://bit.ly/2zd4Lyj", null);
 		}
 		return true;
 	}
